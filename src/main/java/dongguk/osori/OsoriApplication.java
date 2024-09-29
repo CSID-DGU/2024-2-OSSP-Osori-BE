cbd88f02e01cdb34533cd622cb1549151109a425
toni <leesoeun2746@dgu.ac.kr>
2024-09-29 23:59:12 +0900

    feat: 회원가입 및 로그인 기능 구현

--- src/main/java/dongguk/osori/OsoriApplication.java
+++ src/main/java/dongguk/osori/OsoriApplication.java
@@ -1,7 +1,11 @@
 package dongguk.osori;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class OsoriApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(OsoriApplication.class, args);
     }
 }
