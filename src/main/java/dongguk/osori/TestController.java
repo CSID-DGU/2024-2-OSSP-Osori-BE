008333b4ee9d500219a885f0144ae2dabe74b52e
toni (이소은) <144209738+saokiritoni@users.noreply.github.com>
2024-11-03 20:26:59 +0900

    Chore: Test 컨트롤러

---
+++ src/main/java/dongguk/osori/TestController.java
@@ -0,0 +1,12 @@
+package dongguk.osori;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home() {
+        return "CI/CD 성공!";
+    }
+}
